7693b08568638b5a9b898c4a4abeff7fc1f584fc
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2021-11-19 00:08:06 -0500

    fix

--- DataAnalysis/Heatmap/HeatMapGen.py
+++ DataAnalysis/Heatmap/HeatMapGen.py
@@ -7,7 +7,7 @@ from ipywidgets.embed import embed_minimal_html
 # docs: https://jupyter-gmaps.readthedocs.io/en/latest/tutorial.html#heatmaps
 
 API_KEY = "AIzaSyDKHoSJ2IquAFdaqitxPfF2ZsxyNK53siI"
-DIR_PATH = "DataAnalysis/Heatmap/"
+DIR_PATH = "Granatosky/DataAnalysis/Heatmap/"
 data = pd.read_excel(DIR_PATH + "parrot-positional-behavior.xlsx").dropna()
 
 
@@ -17,13 +17,13 @@ def main(WEIGHTED=True):
     locs = get_locations(WEIGHTED)
     center_coords = (40.65231269593503, -73.9905872575025)
     figure_layout = {'width': '1000px', 'height': '1000px', 'margin': '0 auto 0 auto'}
-    fig = gmaps.figure(map_type='HYBRID', center=center_coords ,zoom_level=14, layout=figure_layout)
+    fig = gmaps.figure(map_type='HYBRID', center=center_coords ,zoom_level=16, layout=figure_layout)
 
     path = "WEIGHTED.html"
     if WEIGHTED:
         heatmap_layer = gmaps.heatmap_layer(
         locs[["latitude", "longitude"]], weights=locs["weights"],
-        max_intensity = 100, point_radius = 5.0
+        max_intensity = 50, point_radius = 5.0
         )
     else:
         heatmap_layer = gmaps.heatmap_layer(
@@ -33,7 +33,7 @@ def main(WEIGHTED=True):
         path = "UN-WEIGHTED.html"
 
     fig.add_layer(heatmap_layer)
-    embed_minimal_html(DIR_PATH + "WEIGHTED.html", views=[fig])
+    embed_minimal_html(DIR_PATH + path, views=[fig])
 
 
 
@@ -59,7 +59,6 @@ def get_locations(WEIGHTED=False):
             long = long/1000000
         
         if not (WEIGHTED and weight==0):
-            print(weight)
             locs["latitude"].append(lat)
             locs["longitude"].append(long)
             locs["weights"].append(weight)
